a80b170a3f23e8c368d63e76cc407f1dc2d5aa4f
mstromer <mstromer@gmx.at>
2014-12-04 11:18:10 +0100

    Update censusmap.js

--- block6/censusmap.js
+++ block6/censusmap.js
@@ -29,3 +29,9 @@ $.get("Data/DataDict.txt", function(response) {
       .html(line.substr(10, 105).trim()));
   });
 });
+// Add behaviour to dropdown
+$('#topics').change(function() {
+  wmsLayer.getSource().updateParams({
+    'viewparams': 'column:' + $('#topics>option:selected').val()
+  });
+});
