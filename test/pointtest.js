a6adec3247ad992ae4f61071d87cc2c354d0daf4
edith12345 <gregorides.stefan@gmx.at>
2014-12-17 21:37:44 +0100

    Update pointtest.js

--- test/pointtest.js
+++ test/pointtest.js
@@ -104,13 +104,9 @@ var citybike = new ol.layer.Vector({
   projection: 'EPSG:3857'
 }),
    style: new ol.style.Style({
-  image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-    anchor: [0.5, 46],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-    opacity: 0.75,
-    src: 'data/city.png'
-  }));
+  image: new ol.style.Icon({
+    src: 'data/city.png',
+  });
    });
 }); 
 
