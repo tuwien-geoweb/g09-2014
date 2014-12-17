e0281d93bb9866478b8e4dcbea8841060b3c40eb
edith12345 <gregorides.stefan@gmx.at>
2014-12-17 17:17:56 +0100

    Update pointtest.js

--- test/pointtest.js
+++ test/pointtest.js
@@ -1,6 +1,15 @@
 // Base map
 var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
 
+// Census map layer
+var wmsLayer = new ol.layer.Image({
+  source: new ol.source.ImageWMS({
+    url: 'http://student.ifip.tuwien.ac.at/geoserver/wms',
+    params: {'LAYERS': 'g09_2014:Wien_normalized_6'}
+  }),
+  opacity: 0.6
+});
+
 // Map object
 olMap = new ol.Map({
   target: 'map',
