28c7001cbddc6dbf2455c15af7056a6fc6f17247
edith12345 <gregorides.stefan@gmx.at>
2014-12-17 17:28:07 +0100

    Update pointtest.js

--- test/pointtest.js
+++ test/pointtest.js
@@ -13,7 +13,7 @@ var wmsLayer = new ol.layer.Image({
 // Map object
 olMap = new ol.Map({
   target: 'map',
-  renderer: ol.RendererHint.CANVAS,
+  renderer: 'canvas',
   layers: [osmLayer, wmsLayer],
   view: new ol.View({
     center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
