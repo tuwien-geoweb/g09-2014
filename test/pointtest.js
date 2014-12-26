f41d3665b0f5eb3f18952c144b99351f1dad1a83
edith12345 <gregorides.stefan@gmx.at>
2014-12-26 11:11:15 +0100

    Update pointtest.js

--- test/pointtest.js
+++ test/pointtest.js
@@ -104,8 +104,7 @@ var citybike = new ol.layer.Vector({
   projection: 'EPSG:3857'
 }),
  style: new ol.style.Style({
-      image: 
-        new ol.style.Image({
+       image: new ol.style.Icon({
           url: 'data/city.png',
         })
       
