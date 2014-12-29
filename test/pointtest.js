df8b63e9e555620e9af3975baaf577b265a13f5f
mstromer <mstromer@gmx.at>
2014-12-29 14:59:42 +0100

    Update pointtest.js

--- test/pointtest.js
+++ test/pointtest.js
@@ -14,12 +14,7 @@ var wmsLayer = new ol.layer.Image({
 olMap = new ol.Map({
   target: 'map',
   renderer: 'canvas',
-  layers: [osmLayer, wmsLayer],
-  view: new ol.View({
-    center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
-    zoom: 11,
-    maxZoom: 18
-  })
+  layers: [osmLayer, wmsLayer]
 });
 
 
